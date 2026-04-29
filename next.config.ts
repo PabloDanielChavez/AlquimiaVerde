9f71957c9c8c511f39588b175b0a547866f3dc51
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-04-29 16:49:28 -0300

    Push

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200],
-  },
+  }, 
+  
 };
 
 export default nextConfig; 
